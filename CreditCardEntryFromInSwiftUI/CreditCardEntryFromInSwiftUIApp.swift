6af8553da3055140d2c7960b2fba5c003c413557
ram4ik <ram4ik@hotmail.com>
2024-01-07 09:56:37 +0200

    Initial Commit

---
+++ CreditCardEntryFromInSwiftUI/CreditCardEntryFromInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditCardEntryFromInSwiftUIApp.swift
+//  CreditCardEntryFromInSwiftUI
+//
+//  Created by Ramill Ibragimov on 07.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CreditCardEntryFromInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
